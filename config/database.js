a57984caa59a64e34929e590bd649a90b4d3bee9
tom balcaen <tom.balcaen88@gmail.com>
2019-02-05 11:32:22 +0100

    build

--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
 module.exports = {
     database: "",
     secret: 'yoursecret',
-    connection_uri: 'https://ripe-avocado.herokuapp.com',
+    connection_uri: 'https://happy-plate-academy--staging.herokuapp.com',
     uri : 'mongodb+srv://tombalcaen:updvrf5n@cluster0-a2nr5.gcp.mongodb.net/test?retryWrites=true',
-    mlab_uri : "mongodb://tombalcaen:updvrf5n@ds221003.mlab.com:21003/heroku_90qwhmmx"
+    // mlab_uri : "mongodb://tombalcaen:updvrf5n@ds221003.mlab.com:21003/heroku_90qwhmmx"
+    mlab_uri : "mongodb://tombalcaen:updvrf5n@ds223015.mlab.com:23015/heroku_tcjr4zlq"
 }
+
+// mongodb://<dbuser>:<dbpassword>@ds223015.mlab.com:23015/heroku_tcjr4zlq
